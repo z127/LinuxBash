53629efba7a165dee316468f253366e694617325
zqj <471365897@qq.com>
2021-07-28 10:49:18 +0800

    Update xsync.sh

--- xsync.sh
+++ xsync.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-if [[ $# -lt 1  ]]; then  echo  no param;exit ; fi
-# 必须目标父目录存在,否则会有问题.
-p=$1
-#echo p=$p 
-dir=$(dirname $p)
-#echo dir=$dir
-filename=$(basename $p)
-#echo filename=$filename
-cd -P  $dir 
-fullpath=$(pwd -P .)
-user=$(whoami)
-echo $fullpath
-for(( i = 102 ; i <= 105 ; i = $i + 1 )) ; do
-	echo ========= s$i   $params  =======
-	rsync -lr  $p ${user}@s$i:$fullpath  
-done ;
+## 必须目标fullpath中父目录存在,否则会出现问题
+# 获取输出参数,如果没有参数则直接返回
+pcount=$#
+if [[ $pcount -lt 1  ]]; then  echo "no param";exit ; fi
 
+#获取传输文件名字
+filename=$1
+echo "file name is  ${filename} ! "
+
+#获取文件的绝对路径
+pdir=`cd -P $(dirname ${filename}); pwd`
+echo "file path is ${pdir}"
+
+fullpath=${pdir}
+
+user=$(whoami)
+echo ${fullpath}
+echo ${fullpath}/${filename}
+for(( i = 200 ; i <= 202 ; i = $i + 1 )) ; do
+        echo ========= s$i   $params  =======
+        echo "rsync -lr  ${fullpath}/${filename} ${user}@s$i:${fullpath}"
+        rsync -lr  ${fullpath}/${filename} ${user}@s$i:${fullpath}
+done ;
